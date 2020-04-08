51c2bd760b671c49d7d26404a4d82401a09bbdbf
Victor <32404377+vector-y@users.noreply.github.com>
2020-04-08 11:59:15 -0700

    added promotion view

---
+++ src/Cecs475.BoardGames.Chess.WpfView/ChessPromotionView.xaml.cs
@@ -0,0 +1,44 @@
+﻿using Cecs475.BoardGames.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Cecs475.BoardGames.Chess.WpfView
+{
+    /// <summary>
+    /// Interaction logic for ChessPromotionView.xaml
+    /// </summary>
+    public partial class ChessPromotionView : UserControl
+    {
+        public ChessPromotionView(BoardPosition startPos, BoardPosition endPos)
+        {
+            InitializeComponent();
+        }
+        private void Border_MouseEnter(object sender, MouseEventArgs e)
+        {
+            Border b = sender as Border;
+            var element = (UIElement)e.Source;
+            int col = Grid.GetColumn(element);
+
+        }
+        private void Border_MouseUp(object sender, MouseEventArgs e)
+        {
+
+        }
+        private void Border_MouseLeave(object sender, MouseEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ src/Cecs475.BoardGames.Chess.WpfView/LOL.xaml.cs
@@ -0,0 +1,34 @@
+﻿using Cecs475.BoardGames.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Cecs475.BoardGames.Chess.WpfView
+{
+    /// <summary>
+    /// Interaction logic for ChessPromotionView.xaml
+    /// </summary>
+    public partial class Border_MouseEnter : UserControl
+    {
+
+        public Border_MouseEnter(BoardPosition startPos, BoardPosition endPos)
+        {
+            InitializeComponent();
+        }
+        private Border_MouseEnter()
+        {
+
+        }
+    }
+}
